4d02de5a15de7838b32122c61b6d2462543ba424
Guillem Talayero Carrasco <Guillem>
2022-11-08 20:26:20 +0100

    tabla estadisticas

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 window.onload = (event) => {
-    var array= document.getElementsByName('js-open-modal');
+    var array = document.getElementsByName('js-open-modal');
     console.log(array)
     for (const i in array) {
         console.log(array[i])
-        array[i].addEventListener("click" , function (eve){
+        array[i].addEventListener("click", function(eve) {
             document.getElementById('modal').classList.add('visible')
-        })
+        }, false);
     }
-    console.log(  document.getElementsByClassName('js-close-modal'))
+    console.log(document.getElementsByClassName('js-close-modal'))
 
 
 };
 
 
 function clickMe(id, estado, sala) {
-    var cambio ='si';
-    window.location.href='../view/mostrarMesas.php?sala='+sala + '&est='+estado + '&id=' +id+'&camb='+ cambio
+    var cambio = 'si';
+    window.location.href = '../view/mostrarMesas.php?sala=' + sala + '&est=' + estado + '&id=' + id + '&camb=' + cambio
 }
