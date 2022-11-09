600531f448bdc7a8feae8b196510a1325753109a
Pablo <pablo4goro@gmail.com>
2022-11-09 17:53:56 +0100

    modal inicial

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function clickMe(id, estado, sala) {
 
     var array= document.getElementsByName('js-open-modal');
@@ -14,18 +13,6 @@ function clickMe(id, estado, sala) {
         e.preventDefault();
         modal.classList.remove('modal--show');
     })
-=======
-window.onload = (event) => {
-    var array = document.getElementsByName('js-open-modal');
-    console.log(array)
-    for (const i in array) {
-        console.log(array[i])
-        array[i].addEventListener("click", function(eve) {
-            document.getElementById('modal').classList.add('visible')
-        }, false);
-    }
-    console.log(document.getElementsByClassName('js-close-modal'))
->>>>>>> 4d02de5a15de7838b32122c61b6d2462543ba424
 
     document.getElementById('submit').addEventListener("submit" , function (e){
         var comen = document.getElementById('comensales').value
@@ -36,10 +23,3 @@ window.onload = (event) => {
 
 }
 
-<<<<<<< HEAD
-=======
-function clickMe(id, estado, sala) {
-    var cambio = 'si';
-    window.location.href = '../view/mostrarMesas.php?sala=' + sala + '&est=' + estado + '&id=' + id + '&camb=' + cambio
-}
->>>>>>> 4d02de5a15de7838b32122c61b6d2462543ba424
