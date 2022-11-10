cc10b18fb8a7ab7455d077a219f859cb7eb9aecf
Marc García-Cuevas de Paz <marcg-c20@hotmail.es>
2022-11-10 16:02:49 +0100

    nuevo

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 function clickMe2(id, estado, sala) {
 
-    var array= document.getElementsByName('js-open-modal');
+    var array = document.getElementsByName('js-open-modal');
 
-    for (let i=0; i<array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
 
-        array[i].addEventListener("click" , function (e){
-            let modal= document.querySelector('.modal');
+        array[i].addEventListener("click", function (e) {
+            let modal = document.querySelector('.modal');
             modal.classList.add('modal--show');
         })
     }
-    document.getElementById('js-close-modal').addEventListener("click" , function (e){
+    document.getElementById('js-close-modal').addEventListener("click", function (e) {
         e.preventDefault();
         modal.classList.remove('modal--show');
     })
 
-    document.getElementById('form').addEventListener("submit" , function (e){
+    document.getElementById('form').addEventListener("submit", function (e) {
 
         var comen = document.getElementById('comensales').value
-        var cambio ='si';
+        var cambio = 'si';
 
-        var url="../view/mostrarMesas.php?sala="+sala + "&est="+estado + "&id=" +id+"&camb="+ cambio+"&comen="+comen;
+        var url = "../view/mostrarMesas.php?sala=" + sala + "&est=" + estado + "&id=" + id + "&camb=" + cambio + "&comen=" + comen;
 
-        document.getElementById('form').action=url
+        document.getElementById('form').action = url
 
 
     })
@@ -32,10 +32,10 @@ function clickMe2(id, estado, sala) {
 function clickMe(id, estado, sala) {
 
 
-        var comen = 0
-        var cambio ='si';
-        var url='../view/mostrarMesas.php?sala='+sala + '&est='+estado + '&id=' +id+'&camb='+ cambio+'&comen='+comen;
-        window.location.href=url
+    var comen = 0
+    var cambio = 'si';
+    var url = '../view/mostrarMesas.php?sala=' + sala + '&est=' + estado + '&id=' + id + '&camb=' + cambio + '&comen=' + comen;
+    window.location.href = url
 
 }
 
--- view/principal.php
+++ view/principal.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- LINK BOOTSTRAP -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        <!-- LINK CSS -->
-        <link rel="stylesheet" href="../static/css/styles.css">
-        <!-- LINK JS -->
-        <script type="text/javascript" src="../static/js/script.js"></script>
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <!-- LINK FONT AWESOME -->
-        <script src="https://kit.fontawesome.com/2b5286e1aa.js" crossorigin="anonymous"></script>
-        <title>Página Principal - MPGE</title>
-    </head>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINK BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- LINK CSS -->
+    <link rel="stylesheet" href="../static/css/styles.css">
+    <!-- LINK JS -->
+    <script type="text/javascript" src="../static/js/script.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <!-- LINK FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/2b5286e1aa.js" crossorigin="anonymous"></script>
+    <title>Página Principal - MPGE</title>
+</head>
+
 <body>
     <div class="nav">
         <h1>Gestió Taules - MPGE</h1>
@@ -23,7 +25,7 @@
         <hr class="separador">
     </div>
 
-    <p class="bienvenida">Bienvenido @camarero1</p> 
+    <p class="bienvenida">Bienvenido @camarero1</p>
 
     <div class="menu">
 
@@ -31,13 +33,15 @@
         require_once "../modal/sala.php";
         session_start();
 
-        $ListaSalas=Sala::getAll();
-        foreach ($ListaSalas as $salas => $sala){
+        $ListaSalas = Sala::getAll();
+        foreach ($ListaSalas as $salas => $sala) {
 
             echo "<a href='mostrarmesas.php?sala={$sala['Id_sala']}'>{$sala['nombre_sala']}</a>";
         }
         ?>
         <a href="estadisticas.php">ESTADÍSTICAS</a>
+        <a href="incidencia.php">INCIDENCIAS</a>
     </div>
 </body>
+
 </html>
